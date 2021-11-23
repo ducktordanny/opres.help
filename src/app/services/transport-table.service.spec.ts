acd8206848b34795a6dccba3d0ea7382d05f7c9e
Daniel Lazar <lazardani0112@gmail.com>
2021-11-23 17:01:09 +0100

    Initial commit

---
+++ src/app/services/transport-table.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { TransportTableService } from './transport-table.service';
+
+describe('TransportTableService', () => {
+  let service: TransportTableService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(TransportTableService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
