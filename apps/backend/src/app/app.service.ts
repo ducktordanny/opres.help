e80032001dac220374b668145e10e1676033bebd
Daniel Lazar <lazardani0112@gmail.com>
2022-03-01 08:20:33 +0100

    refactor(backend): Rename api -> backend

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -4,6 +4,6 @@ import {Message} from '@opres/api-interfaces';
 @Injectable()
 export class AppService {
   getData(): Message {
-    return {message: 'Welcome to api!'};
+    return {message: 'Welcome to backend!'};
   }
 }
