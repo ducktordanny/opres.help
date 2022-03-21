83f07b179ff8cecc0b7f9f1b818ffb12b2a55b71
Daniel Lazar <lazardani0112@gmail.com>
2022-03-21 08:11:13 +0100

    refact(table-helper): move table creator functions to shared

--- apps/frontend/src/app/components/input-table/input-table.component.ts
+++ apps/frontend/src/app/components/input-table/input-table.component.ts
@@ -7,12 +7,14 @@ import {
 } from '@angular/core';
 
 import {InputTableService} from '@components/input-table/input-table.service';
+import {
+  rowDefinitionsFrom,
+  tableSourceFrom,
+} from '@shared/helpers/table.helper';
+import {RowDefs, Table} from '@shared/types/table.types';
 import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
 import {BehaviorSubject, combineLatest} from 'rxjs';
 import {filter, map, tap} from 'rxjs/operators';
-
-export type Table = Array<{[key: string]: number | null}>;
-type RowDefs = Array<string>;
 
 @UntilDestroy()
 @Component({
@@ -27,7 +29,7 @@ export class InputTableComponent {
   public readonly columns$ = new BehaviorSubject<number>(1);
   public readonly rowDefinitions$ = new BehaviorSubject<RowDefs>([]);
   public readonly tableSource$ = new BehaviorSubject<Table>([]);
-  private hasValues = new BehaviorSubject<boolean>(false);
+  private hasValues$ = new BehaviorSubject<boolean>(false);
 
   @Input() set rows(value: number | null) {
     if (value) this.rows$.next(value);
@@ -41,30 +43,31 @@ export class InputTableComponent {
     combineLatest([this.rows$, this.columns$])
       .pipe(
         map(([rows, columns]) => ({
-          rowDefinitions: this.rowDefinitionsFrom(columns),
+          rowDefinitions: rowDefinitionsFrom(columns),
           rows,
         })),
         map(({rowDefinitions, rows}) => {
           this.rowDefinitions$.next(rowDefinitions);
-          return this.tableSourceFrom(rows, rowDefinitions);
+          return tableSourceFrom(rows, rowDefinitions);
         }),
         tap((tableSource) => this.tableSource$.next(tableSource)),
         untilDestroyed(this),
       )
       .subscribe();
+
     this.inputTableService.clear$
       .pipe(
-        filter(() => this.hasValues.getValue()),
+        filter(() => this.hasValues$.getValue()),
         filter((keys) => keys.some((key) => key === this.key)),
         map(() =>
-          this.tableSourceFrom(
+          tableSourceFrom(
             this.rows$.getValue(),
             this.rowDefinitions$.getValue(),
           ),
         ),
         tap((newTableSource) => {
           this.tableSource$.next(newTableSource);
-          this.hasValues.next(false);
+          this.hasValues$.next(false);
         }),
         untilDestroyed(this),
       )
@@ -76,18 +79,6 @@ export class InputTableComponent {
     currentTable[row][column] = +(<HTMLInputElement>event?.target)?.value;
     this.tableSource$.next(currentTable);
     this.tableChange.emit(currentTable);
-    this.hasValues.next(true);
+    this.hasValues$.next(true);
   }
-
-  private tableSourceFrom(rows: number, rowDefinitions: RowDefs): Table {
-    const rowContent = rowDefinitions.reduce(
-      (acc, curr) => ({...acc, [curr]: null}),
-      {},
-    );
-    // the spreading is necessary to not create reference between rows
-    return Array.from({length: rows}, () => ({...rowContent}));
-  }
-
-  private rowDefinitionsFrom = (columns: number): RowDefs =>
-    Array.from({length: columns}, (_, index) => `${index}`);
 }
