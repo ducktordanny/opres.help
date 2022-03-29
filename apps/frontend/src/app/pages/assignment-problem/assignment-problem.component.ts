5543a1c4afe37d61ed9be32ca190fd549b6f4532
Daniel Lazar <lazardani0112@gmail.com>
2022-03-29 10:25:11 +0200

    feat(assignment-problem): create new page

---
+++ apps/frontend/src/app/pages/assignment-problem/assignment-problem.component.ts
@@ -0,0 +1,9 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  selector: 'assignment-problem-page',
+  templateUrl: './assignment-problem.template.html',
+  styles: [``],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AssignmentProblemComponent {}
--- apps/frontend/src/app/pages/assignment-problem/assignment-problem.module.ts
+++ apps/frontend/src/app/pages/assignment-problem/assignment-problem.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from '@angular/core';
 
+import {InputTableModule} from "@components/input-table/input-table.module";
+
 import {AssignmentProblemPageComponent} from './assignment-problem.page';
 import {AssignmentProblemRouting} from './assignment-problem.routing';
 
 @NgModule({
   declarations: [AssignmentProblemPageComponent],
-  imports: [AssignmentProblemRouting],
+  imports: [AssignmentProblemRouting, InputTableModule],
 })
 export class AssignmentProblemModule {}
