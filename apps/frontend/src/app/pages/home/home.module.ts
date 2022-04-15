e2901b4ae63a5d4646a8850a149ecccd195537e3
Daniel Lazar <lazardani0112@gmail.com>
2022-04-15 18:50:03 +0200

    feat(routing): lazy load pages

--- apps/frontend/src/app/pages/home/home.module.ts
+++ apps/frontend/src/app/pages/home/home.module.ts
@@ -1,8 +1,11 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 
 import {HomeComponent} from './home.component';
+import {HomeRouteModule} from './home.routing.module';
 
 @NgModule({
   declarations: [HomeComponent],
+  imports: [CommonModule, HomeRouteModule],
 })
 export class HomeModule {}
