02e6bbfb87cb064019c080ffc323a3b34d939b4d
Daniel Lazar <lazardani0112@gmail.com>
2022-04-23 12:09:38 +0200

    feat(not-found): style page

--- apps/frontend/src/app/pages/not-found/not-found.page.ts
+++ apps/frontend/src/app/pages/not-found/not-found.page.ts
@@ -2,7 +2,15 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 
 @Component({
   selector: 'not-found-page',
-  template: ` <p>404. Page Not Found</p> `,
+  template: `
+    <h1>Ooops...</h1>
+    <img
+      class="not-found-icon"
+      src="../../../assets/icons/not-found.icon.svg"
+      alt="Not Found Icon"
+    />
+  `,
+  styleUrls: ['./not-found.style.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NotFoundPageComponent {}
