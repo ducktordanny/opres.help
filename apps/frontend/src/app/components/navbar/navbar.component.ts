30f9ca54061b56e05e8673342871b2eacbf77cfb
Daniel Lazar <lazardani0112@gmail.com>
2022-04-24 20:34:59 +0200

    refactor(navbar): remove unnecessary navbar component

--- apps/frontend/src/app/components/navbar/navbar.component.ts
+++
@@ -1,9 +0,0 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-
-@Component({
-  selector: 'navbar',
-  templateUrl: './navbar.template.html',
-  styleUrls: ['./navbar.style.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class NavbarComponent {}
