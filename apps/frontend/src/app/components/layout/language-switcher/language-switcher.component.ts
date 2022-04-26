c9042b40ba7d0d55cbebc6ecb9ba3a8406227dc1
Daniel Lazar <lazardani0112@gmail.com>
2022-04-26 22:36:14 +0200

    feat(language-switcher): implement basic functionality

--- apps/frontend/src/app/components/layout/language-switcher/language-switcher.component.ts
+++ apps/frontend/src/app/components/layout/language-switcher/language-switcher.component.ts
@@ -1,8 +1,30 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'language-switcher',
   templateUrl: './language-switcher.template.html',
+  styleUrls: ['../layout.style.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LanguageSwitcherComponent {}
+export class LanguageSwitcherComponent implements OnInit {
+  public readonly LANGUAGES = [
+    {id: 'eng', label: '🇬🇧 Angol'},
+    {id: 'hun', label: '🇭🇺 Magyar'},
+  ];
+  public selectedLanguage =
+    localStorage.getItem('user.selectedLanguage') || 'eng';
+
+  public ngOnInit(): void {
+    this.changeLanguage();
+  }
+
+  public onLanguageSelect(langId: string): void {
+    this.selectedLanguage = langId;
+    this.changeLanguage();
+  }
+
+  private changeLanguage(): void {
+    localStorage.setItem('user.selectedLanguage', this.selectedLanguage);
+    // ...
+  }
+}
