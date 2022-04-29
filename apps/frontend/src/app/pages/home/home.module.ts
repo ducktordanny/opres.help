47136033d167179ab26c3aae1f5625c1809f9025
Daniel Lazar <lazardani0112@gmail.com>
2022-04-30 00:00:22 +0200

    feat(home-page): hide sidebar on home & add basic texts

--- apps/frontend/src/app/pages/home/home.module.ts
+++ apps/frontend/src/app/pages/home/home.module.ts
@@ -1,11 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+import {TranslateModule} from '@ngx-translate/core';
 
 import {HomePageComponent} from './home.page';
 import {HomeRouteModule} from './home.routing';
 
 @NgModule({
   declarations: [HomePageComponent],
-  imports: [CommonModule, HomeRouteModule],
+  imports: [CommonModule, HomeRouteModule, MatFormFieldModule, TranslateModule],
 })
 export class HomeModule {}
