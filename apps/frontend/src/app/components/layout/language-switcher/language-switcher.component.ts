5a02bf0ce3b963bad67ee214adb76c2ee7dad6da
Daniel Lazar <lazardani0112@gmail.com>
2022-08-22 23:17:23 +0200

    refactor(language-switcher): wrap necessary logic into service
    - ngx-translate does not provide an Observable where the current language would be available so it's been done in here

--- apps/frontend/src/app/components/layout/language-switcher/language-switcher.component.ts
+++ apps/frontend/src/app/components/layout/language-switcher/language-switcher.component.ts
@@ -1,6 +1,10 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 
-import {TranslateService} from '@ngx-translate/core';
+import {
+  Language,
+  LanguageSwitcherService,
+} from '@frontend/components/layout/language-switcher/language-switcher.service';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'language-switcher',
@@ -8,24 +12,17 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['../layout.style.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LanguageSwitcherComponent implements OnInit {
-  public readonly LANGUAGES = [
+export class LanguageSwitcherComponent {
+  public readonly LANGUAGES: Array<{id: Language; label: string}> = [
     {id: 'en', label: 'LANGUAGE.EN'},
     {id: 'hu', label: 'LANGUAGE.HU'},
   ];
-  public selectedLanguage = localStorage.getItem('user.selectedLanguage');
+  public selectedLanguage$: Observable<Language> =
+    this.languageSwitcherService.currentLanguage;
 
-  constructor(private translateService: TranslateService) {}
+  constructor(private languageSwitcherService: LanguageSwitcherService) {}
 
-  public ngOnInit(): void {
-    if (!this.selectedLanguage)
-      this.selectedLanguage = this.translateService.getDefaultLang();
-    else this.translateService.use(this.selectedLanguage);
-  }
-
-  public onLanguageSelect(langId: string): void {
-    this.selectedLanguage = langId;
-    localStorage.setItem('user.selectedLanguage', langId);
-    this.translateService.use(langId);
+  public onLanguageSelect(langId: Language): void {
+    this.languageSwitcherService.changeLanguage(langId);
   }
 }
