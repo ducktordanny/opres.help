4c92b471c1141543d0bf5f1bf6082d2fec6163dc
Daniel Lazar <lazardani0112@gmail.com>
2023-01-26 18:20:52 +0100

    feat(assignment-problem): refactor service to use the new endpoints

--- apps/frontend/src/app/pages/assignment-problem/assignment-problem.service.ts
+++ apps/frontend/src/app/pages/assignment-problem/assignment-problem.service.ts
@@ -1,23 +1,75 @@
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
-import {Table} from '@opres/shared/types';
+import {
+  AssignmentProblemType,
+  HungarianMethodResponse,
+  KoenigAlgoResponse,
+  ReduceResponse,
+  SelectedCell,
+  Table,
+  ZeroFindingMethod,
+} from '@opres/shared/types';
 import {ErrorHandlerService} from '@frontend/services/error-handler.service';
-import {catchError} from 'rxjs';
+import {catchError, Observable, Subject} from 'rxjs';
+import {Cacheable} from 'ts-cacheable';
+
+const assignmentProblemCacheBuster = new Subject<void>();
 
 @Injectable()
 export class AssignmentProblemService {
-  constructor(
-    private http: HttpClient,
-    private errorHandler: ErrorHandlerService,
-  ) {}
+  constructor(private http: HttpClient, private errorHandler: ErrorHandlerService) {}
 
-  public calculateFirsPhase(assignmentTable: Table) {
+  @Cacheable({cacheBusterObserver: assignmentProblemCacheBuster})
+  public calculateAll(
+    assignmentTable: Table,
+    problemType = AssignmentProblemType.Min,
+    method = ZeroFindingMethod.Greedy,
+  ): Observable<HungarianMethodResponse> {
+    const params = new HttpParams().set('zero-finding-method', method).set('type', problemType);
+
     return this.http
-      .post<Table>(this.urlPrefix('first-phase'), assignmentTable)
+      .post<HungarianMethodResponse>(this.urlPrefix(), assignmentTable, {params})
       .pipe(catchError(this.errorHandler.showError));
   }
 
-  private urlPrefix = (path: string): string =>
-    `/api/assignment-problem/${path}`;
+  @Cacheable({cacheBusterObserver: assignmentProblemCacheBuster})
+  public calculateKoenigAlgorithm(
+    assignmentTable: Table,
+    method = ZeroFindingMethod.Greedy,
+  ): Observable<KoenigAlgoResponse> {
+    const url = this.urlPrefix('koenig-algorithm');
+    const params = new HttpParams().set('zero-finding-method', method);
+
+    return this.http
+      .post<KoenigAlgoResponse>(url, assignmentTable, {params})
+      .pipe(catchError(this.errorHandler.showError));
+  }
+
+  @Cacheable({cacheBusterObserver: assignmentProblemCacheBuster})
+  public calculateIndependentZeros(
+    assignmentTable: Table,
+    method = ZeroFindingMethod.Greedy,
+  ): Observable<Array<SelectedCell>> {
+    const url = this.urlPrefix(`independent-zeros/${method}`);
+
+    return this.http
+      .post<Array<SelectedCell>>(url, assignmentTable)
+      .pipe(catchError(this.errorHandler.showError));
+  }
+
+  @Cacheable({cacheBusterObserver: assignmentProblemCacheBuster})
+  public reduce(
+    assignmentTable: Table,
+    problemType = AssignmentProblemType.Min,
+  ): Observable<ReduceResponse> {
+    const url = this.urlPrefix('reduce');
+    const params = new HttpParams().set('type', problemType);
+
+    return this.http
+      .post<ReduceResponse>(url, assignmentTable, {params})
+      .pipe(catchError(this.errorHandler.showError));
+  }
+
+  private urlPrefix = (path?: string): string => `/api/assignment-problem/${path || ''}`;
 }
