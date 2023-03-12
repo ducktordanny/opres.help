8f2e00cb36336512e3b834688cce3c77c82b8354
Daniel Lazar <lazardani0112@gmail.com>
2023-03-12 21:21:51 +0100

    build(backend): add & setup basic swagger config

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,18 +5,30 @@
 
 import {Logger} from '@nestjs/common';
 import {NestFactory} from '@nestjs/core';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+
+import constants from '@opres/shared/data/constants';
 
 import {AppModule} from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  const swaggerPrefix = globalPrefix + '/swagger';
   const port = process.env.PORT || 3333;
+
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Opres API')
+    .setDescription('The opres.help public API endpoints.')
+    .setVersion(constants.appVersion)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(swaggerPrefix, app, document);
+
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
 
 bootstrap();
