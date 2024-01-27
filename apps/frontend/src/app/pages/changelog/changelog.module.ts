46cf52f71e53e2fe10558bac3cd1ea662af1fb08
Daniel Lazar <lazardani0112@gmail.com>
2024-01-28 00:20:31 +0100

    feat(changelog): add language info snackbar

--- apps/frontend/src/app/pages/changelog/changelog.module.ts
+++ apps/frontend/src/app/pages/changelog/changelog.module.ts
@@ -1,6 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 import {MdPreviewModule} from '../../pipes/md-preview/md-preview.module';
 
@@ -11,7 +12,13 @@ import {ChangelogService} from './changelog.service';
 @NgModule({
   declarations: [ChangelogPageComponent],
   providers: [ChangelogService],
-  imports: [ChangelogRouteModule, CommonModule, HttpClientModule, MdPreviewModule],
+  imports: [
+    ChangelogRouteModule,
+    CommonModule,
+    HttpClientModule,
+    MatSnackBarModule,
+    MdPreviewModule,
+  ],
   exports: [],
 })
 export class ChangelogPageModule {}
--- apps/frontend/src/app/pages/changelog/changelog.page.ts
+++ apps/frontend/src/app/pages/changelog/changelog.page.ts
@@ -1,10 +1,13 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component} from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
+import {UntilDestroy, untilDestroyed} from '@ngneat/until-destroy';
+import {TranslateService} from '@ngx-translate/core';
 import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
 
 import {ChangelogService} from './changelog.service';
 
+@UntilDestroy()
 @Component({
   templateUrl: './changelog.template.html',
   styles: [
@@ -16,12 +19,29 @@ import {ChangelogService} from './changelog.service';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ChangelogPageComponent {
+export class ChangelogPageComponent implements AfterViewInit {
   public changelogContent: Observable<string>;
 
-  constructor(private changelogService: ChangelogService) {
-    this.changelogContent = this.changelogService
-      .getChangelogContent()
-      .pipe(tap((value) => console.log(value)));
+  constructor(
+    private changelogService: ChangelogService,
+    private snackBar: MatSnackBar,
+    private translateService: TranslateService,
+  ) {
+    this.changelogContent = this.changelogService.getChangelogContent();
+  }
+
+  public ngAfterViewInit(): void {
+    this.showLanguageInfo();
+    this.translateService.onLangChange
+      .pipe(untilDestroyed(this))
+      .subscribe(() => this.showLanguageInfo());
+  }
+
+  public showLanguageInfo(): void {
+    const snackBarContent = this.translateService.instant('CHANGELOG.INFO');
+    const actionContent = this.translateService.instant('CLOSE');
+    this.snackBar.open(snackBarContent, actionContent, {
+      duration: 5_000,
+    });
   }
 }
